f7717422885d3068dfefb1e48449865280471001
JamesChapmanNV <89288003+JamesChapmanNV@users.noreply.github.com>
2022-03-18 22:41:38 -0500

    First jawsdb

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=x7sd1lczpn9hja36';
+    $username = 'i3j6yd9q6r9nql63';
+    $password = 'lwwmobf6kr2oer9u';
 
     try {
         $db = new PDO($dsn, $username, $password);
